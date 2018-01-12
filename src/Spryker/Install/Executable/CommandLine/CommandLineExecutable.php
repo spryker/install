d2d21090f66532b7584544bb7b752b6905fff7f1
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2018-01-12 14:30:34 +0000

    Fix broken process environment variable

--- src/Spryker/Install/Executable/CommandLine/CommandLineExecutable.php
+++ src/Spryker/Install/Executable/CommandLine/CommandLineExecutable.php
@@ -43,7 +43,7 @@ class CommandLineExecutable implements ExecutableInterface
      */
     public function execute(StyleInterface $output): int
     {
-        $process = new Process($this->command->getExecutable(), SPRYKER_ROOT, null, null, 600);
+        $process = new Process($this->command->getExecutable(), SPRYKER_ROOT, getenv(), null, 600);
         $process->start();
 
         foreach ($process as $buffer) {
